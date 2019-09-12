75884cadba46398bef376fa0d5da5111d173991c
SUYOUNG KIM <suyoung154@gmail.com>
2019-09-12 14:07:22 +0900

    koa base settings

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,10 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-  ctx.body = 'hello world';
-});
+app.use(bodyParser());
+
+router.use('/api', api.routes());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
