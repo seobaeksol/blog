0acab087432a3a1453330bb93d08276fd99670f5
SUYUONG KIM <suyoung154@gmail.com>
2019-09-15 14:10:40 +0900

    login

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -46,7 +46,30 @@ export const register = async ctx => {
   }
 };
 
-export const login = async ctx => {};
+export const login = async ctx => {
+  const { username, password } = ctx.request.body;
+
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 export const check = async ctx => {};
 
