b5739d5f815320cd5da62ccbbefaf032ef032d65
kimpuepue <suyoung154@gmail.com>
2019-09-16 09:36:22 +0900

    logout

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -92,4 +92,7 @@ export const check = async ctx => {
   ctx.body = user;
 };
 
-export const logout = async => {};
+export const logout = async ctx => {
+  ctx.cookies.set('access_token');
+  ctx.status = 204;
+};
