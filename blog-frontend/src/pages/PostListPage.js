edb90aec47f460e8ba3a47404d103fcc7d6cd184
kimpuepue <suyoung154@gmail.com>
2019-09-16 15:38:54 +0900

    global style

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const PostListPage = () => {
-  return <div>Post List</div>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default PostListPage;
