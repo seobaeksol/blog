5cf835684ea2d4bffcc48493d1a626a9d13e1e66
kimpuepue <suyoung154@gmail.com>
2019-09-16 10:58:59 +0900

    apply authentication

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,13 +4,33 @@ import Joi from 'Joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
     return;
   }
+
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      ctx.status = 404;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
   return next();
+};
+
+export const checkOwnPost = async (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (user._id !== post.user._id.toString()) {
+    ctx.status = 403;
+    return;
+  }
+  next();
 };
 
 /* Write Post
@@ -37,11 +57,13 @@ export const write = async ctx => {
     return;
   }
 
+  console.log(ctx.state.user);
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
 
   try {
@@ -87,17 +109,7 @@ export const list = async ctx => {
 GET /api/posts/:id
 */
 export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 /* Delete a post
