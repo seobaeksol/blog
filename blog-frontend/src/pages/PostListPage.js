e66be1db2b4c1b6d5d9e7108a0441242ba4a21d6
kimpuepue <suyoung154@gmail.com>
2019-09-16 11:41:53 +0900

    Basic Page Form

---
+++ blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostListPage = () => {
+  return <div>Post List</div>;
+};
+
+export default PostListPage;
---
+++ blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WritePage = () => {
+  return <div>Write</div>;
+};
+
+export default WritePage;
