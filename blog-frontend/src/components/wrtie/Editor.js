78119d28e1cb19e45f7c26702756feeea4ac4f8f
kimpuepue <suyoung154@gmail.com>
2019-09-17 20:22:09 +0900

    apply quill

---
+++ blog-frontend/src/components/wrtie/Editor.js
@@ -0,0 +1,64 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import Responsive from '../common/Responsive';
+
+const EditorBlock = styled(Responsive)`
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[4]};
+  margin-bottom: 2rem;
+  width: 100%;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor.ql-blank::before {
+    left: 0px;
+  }
+`;
+
+const Editor = () => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: 'Write content...',
+      modules: {
+        // https://quillljs.com/docs/modules/toolbar
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+  }, []);
+
+  return (
+    <EditorBlock>
+      <TitleInput placeholder="Insert Title Here." />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
+};
+
+export default Editor;
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Responsive from '../components/common/Responsive';
+import Editor from '../components/wrtie/Editor';
 
 const WritePage = () => {
-  return <div>Write</div>;
+  return (
+    <Responsive>
+      <Editor />
+    </Responsive>
+  );
 };
 
 export default WritePage;
