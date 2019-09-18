a536ea38acc3df1289848cbe581fa11627fa6b2f
kimpuepue <suyoung154@gmail.com>
2019-09-19 08:50:12 +0900

    build

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -4,6 +4,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import jwtMiddleware from './lib/jwtMiddleware';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -30,6 +33,14 @@ app.use(jwtMiddleware);
 
 router.use('/api', api.routes());
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
